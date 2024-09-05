776fe36d1e4f155129ce80483d8231a7b513ca3f
mido01112 <smyr26135@gmail.com>
2024-09-05 06:52:30 +0200

    Update معلومات-المطور.js

--- plugins/معلومات-المطور.js
+++ plugins/معلومات-المطور.js
@@ -47,14 +47,14 @@ let bjir = 'https://i.imgur.com/RbaRjrb.jpeg'
 let name = await conn.getName(who)
 
 let delay = time => new Promise(res => setTimeout(res, time))
-await conn.sendContact(m.chat, [[`201145624848` + `@s.whatsapp.net`, wm]], vn, {
+await conn.sendContact(m.chat, [[`249111230420` + `@s.whatsapp.net`, wm]], vn, {
  contextInfo: { 
  forwardingScore: 2023,
 isForwarded: false, 
  externalAdReply: {  
  title: '𝑇𝛨𝛯 𝐿𝛩𝛻𝛯𝐿𝑌 𝛩𝑊𝛮𝛯𝑅 𝛩𝐹', 
  body: '𝕊ℍ𝔸𝕎𝔸ℤ𝔸-𝔹𝕆𝕋', 
- sourceUrl: 'https://whatsapp.com/channel/0029Vael6wMJP20ze3IXJk0z',
+ sourceUrl: 'https://chat.whatsapp.com/GwpVRwW4o8z11bctqm9n7Q',
  thumbnail: vn2,
  thumbnailUrl: vn2, 
  mediaType: 1,
